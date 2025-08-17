1f47157fc62e1910a49c6dec034a6f0862341bfb
RazMkhitaryan <razo96@mail.ru>
2025-08-18 02:24:31 +0400

    fix run

--- src/test/java/main/TestBase.java
+++ src/test/java/main/TestBase.java
@@ -1,14 +1,19 @@
 package main;
 
 import factory.WebDriverFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.AllureUtils;
 import utils.VideoUtils;
 
-public abstract class TestBase {
+@SpringBootTest(classes = Application.class)
+public abstract class TestBase extends AbstractTestNGSpringContextTests {
 
-  protected WebDriverFactory webDriverFactory = new WebDriverFactory();
+  @Autowired
+  private WebDriverFactory webDriverFactory;
 
   @BeforeMethod
   public void setUp() throws Exception {
